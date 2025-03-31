9ade84d86debfb13563ecb0ec42a75d6026b0737
Jan Herscheid <jan.herscheid@gmail.com>
2025-04-01 00:48:43 +0200

    remove boilerplate

---
+++ frontend/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>wot stats app (dit is index.tsx)</Text>
+    </View>
+  );
+}
