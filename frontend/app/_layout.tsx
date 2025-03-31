636fd10a5af0a6aef08a371c4ee6551909c4d701
Jan Herscheid <jan.herscheid@gmail.com>
2025-04-01 01:33:08 +0200

    add navbar

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return(
 	<Stack>
-		<Stack.Screen name="index" options={{ title: 'Home' }} />
-		<Stack.Screen name="settings" options={{ title: 'Settings' }} />
+		<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      	<Stack.Screen name="+not-found" />
 	</Stack>
   )
 }
