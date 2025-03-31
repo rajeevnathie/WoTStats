b2e5cb3cea0244c5141717f148eeceff490c1dde
Jan Herscheid <jan.herscheid@gmail.com>
2025-04-01 01:09:15 +0200

    add settings page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>wot stats app (dit is index.tsx)</Text>
+	  <Link href="/settings" style={styles.button}> Settings screen </Link>
     </View>
   );
 }
+
+//This will be removed later XDDD
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
